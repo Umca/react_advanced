22e38269b2876d4d5d241113ef62fdd77fcfc2b5
Olya Akinshyna <olga.akinshyna@gmail.com>
2017-07-11 18:18:31 +0300

    test component added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log(111);
+import React from 'react';
+import {render} from 'react-dom';
+import TestComponent from './TestComponent';
+
+render(
+    <TestComponent />,
+    document.getElementById('mount_place')
+)
