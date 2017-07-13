8532e6a23c0aa420307de0148782836a862e6358
Olya Akinshyna <olga.akinshyna@gmail.com>
2017-07-13 16:55:58 +0300

    middleware added. a little bit of redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import Chat from './Chat';
 import rootReducer from './reducers';
+import {addNewUser} from './actions';
+import reduxThunk from 'redux-thunk';
 
+const loggerMiddleware = (store) => {
+    return function(next){
+        return function (action){
+            console.debug('trigger', action)
+            return next(action);
+        }
+    }
+}
 
-const store = createStore(rootReducer);
+const checkUserMW = store => next=> action =>{
+    if(action.type ==='CONNECTION_RESOLVE'){
+        let fakeRequest = () => new Promise(resolve => {
+            setTimeout(()=> {
+                resolve();
+            }, 2000)
+        })
+
+        fakeRequest().then(()=>{
+            store.dispatch(addNewUser())
+        })
+    }
+    return next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk, loggerMiddleware));
+
 
 window.store = store;
 
--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,27 @@
 import faker from 'faker';
 
 
+// export const addNewUser = () => {
+//     const rndUsername = `@${faker.internet.userName().toLowerCase()}`;
+//     return {
+//         type: 'ADD_NEW_USER',
+//         username: rndUsername
+//     }
+// }
+
 export const addNewUser = () => {
     const rndUsername = `@${faker.internet.userName().toLowerCase()}`;
-    return {
-        type: 'ADD_NEW_USER',
-        username: rndUsername
+    return dispatch => {
+        dispatch({
+            type: 'ADD_NEW_USER',
+            username: rndUsername
+        })
+        dispatch(addNewMessage(
+            rndUsername,
+            'Hi there',
+            Date.now(),
+            
+        ))
     }
 }
 
